c41e288e4833183a6dac6615e79ff77cb91e1995
Swayam Rabari <148040323+SwayamRabari@users.noreply.github.com>
2024-08-18 18:44:46 +0530

    chore: Remove unnecessary redirect option in login route and update login page redirection

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -15,6 +15,7 @@ export async function POST(request: NextRequest) {
     const user = await signIn('credentials', {
       email,
       password,
+      redirect: false,
     });
 
     return NextResponse.json(user);
