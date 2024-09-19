255eac3d7485fde38f489582a4cd460e1b666c45
Swayam Rabari <148040323+SwayamRabari@users.noreply.github.com>
2024-09-19 11:09:09 +0530

    chore: Remove test page component and associated Tiptap editor

--- src/app/testpage/page.tsx
+++
@@ -1,9 +0,0 @@
-import Tiptap from '@/components/editor';
-
-export default function page() {
-  return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <Tiptap />
-    </div>
-  );
-}
